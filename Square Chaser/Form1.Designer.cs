3d122920cdf2d4d013103e0630d7f36530aaaa6e
trenmcki071 <trenmcki071@outlook.com>
2021-06-03 10:05:12 -0400

    Add project files.

---
+++ Square Chaser/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Square_Chaser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.winLabel = new System.Windows.Forms.Label();
+            this.score1Label = new System.Windows.Forms.Label();
+            this.score2Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // winLabel
+            // 
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(12, 9);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(298, 236);
+            this.winLabel.TabIndex = 0;
+            this.winLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // score1Label
+            // 
+            this.score1Label.BackColor = System.Drawing.Color.Transparent;
+            this.score1Label.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.score1Label.Location = new System.Drawing.Point(14, 9);
+            this.score1Label.Name = "score1Label";
+            this.score1Label.Size = new System.Drawing.Size(100, 23);
+            this.score1Label.TabIndex = 1;
+            this.score1Label.Text = "0";
+            this.score1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // score2Label
+            // 
+            this.score2Label.BackColor = System.Drawing.Color.Transparent;
+            this.score2Label.ForeColor = System.Drawing.Color.LightGreen;
+            this.score2Label.Location = new System.Drawing.Point(210, 9);
+            this.score2Label.Name = "score2Label";
+            this.score2Label.Size = new System.Drawing.Size(100, 23);
+            this.score2Label.TabIndex = 2;
+            this.score2Label.Text = "0";
+            this.score2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(322, 254);
+            this.Controls.Add(this.score2Label);
+            this.Controls.Add(this.score1Label);
+            this.Controls.Add(this.winLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Label score1Label;
+        private System.Windows.Forms.Label score2Label;
+    }
+}
+
---
+++ Square Chaser/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Square_Chaser
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(151, 137, 20, 20);
+        Rectangle player2 = new Rectangle(151, 97, 20, 20);
+        Rectangle boost = new Rectangle(10, 126, 10, 10);
+        Rectangle point = new Rectangle(297, 126, 15, 15);
+
+        Random rnd = new Random();
+
+        SoundPlayer boostSound = new SoundPlayer(Properties.Resources.mixkit_explainer_video_game_alert_sweep_236);
+        SoundPlayer pointSound = new SoundPlayer(Properties.Resources.mixkit_video_game_retro_click_237);
+        SoundPlayer winSound = new SoundPlayer(Properties.Resources.mixkit_player_recharging_in_video_game_2041);
+
+
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush greenBrush = new SolidBrush(Color.LightGreen);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+
+        int player1Score;
+        int player2Score;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool aLeft = false;
+        bool dRight = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool LeftArrowLeft = false;
+        bool RightArrowRight = false;
+
+        int player1Speed = 4;
+        int player2Speed = 4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(greenBrush, player2);
+            e.Graphics.FillRectangle(blueBrush, player1);
+            e.Graphics.FillRectangle(yellowBrush, boost);
+            e.Graphics.FillRectangle(whiteBrush, point);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aLeft = true;
+                    break;
+                case Keys.D:
+                    dRight = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Right:
+                    RightArrowRight = true;
+                    break;
+                case Keys.Left:
+                    LeftArrowLeft = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aLeft = false;
+                    break;
+                case Keys.D:
+                    dRight = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Right:
+                    RightArrowRight = false;
+                    break;
+                case Keys.Left:
+                    LeftArrowLeft = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+             
+
+            //making the random x and y values for random boost and point locations
+            int rndX = rnd.Next(10, 297);
+            int rndY = rnd.Next(10, 224);
+
+            //increasing speed with boost and relocating boost
+            if (player1.IntersectsWith(boost))
+            {
+                player1Speed++;
+                boost.X = rndX;
+                boost.Y = rndY;
+                boostSound.Play();
+            }
+
+            if (player2.IntersectsWith(boost))
+            {
+                player2Speed++;
+                boost.X = rndX;
+                boost.Y = rndY;
+                boostSound.Play();
+            }
+
+            //adding points and relocating point
+            if (player1.IntersectsWith(point))
+            {
+                point.X = rndX;
+                point.Y = rndY;
+                player1Score++;
+                score1Label.Text = $"{player1Score}";
+                pointSound.Play();
+            }
+
+            if (player2.IntersectsWith(point))
+            {
+                point.X = rndX;
+                point.Y = rndY;
+                player2Score++;
+                score2Label.Text = $"{player2Score}";
+                pointSound.Play();
+            }
+
+            //checks for game over
+            if (player1Score == 7)
+            {
+                gameTimer.Enabled = false;
+                winLabel.Visible = true;
+                winLabel.Text = "Blue Wins!!";
+                winSound.Play();
+            }
+
+            if (player2Score == 7)
+            {
+                gameTimer.Enabled = false;
+                winLabel.Visible = true;
+                winLabel.Text = "Green Wins!!";
+                winSound.Play();
+            }
+
+            //movement
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= player1Speed;
+            }
+
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += player1Speed;
+            }
+
+            if (aLeft == true && player1.X > 0)
+            {
+                player1.X -= player1Speed;
+            }
+
+            if (dRight == true && player1.X < this.Width - player1.Width)
+            {
+                player1.X += player1Speed;
+            }
+
+            if (upArrowDown == true && player2.Y > 0)
+            {
+                player2.Y -= player2Speed;
+            }
+
+            if (downArrowDown == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += player2Speed;
+            }
+
+            if (RightArrowRight == true && player2.X < this.Width - player2.Width)
+            {
+                player2.X += player2Speed;
+            }
+
+            if (LeftArrowLeft == true && player2.X > 0)
+            {
+                player2.X -= player2Speed;
+            }
+
+            Refresh();
+        }
+    }
+}
